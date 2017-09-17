2c19a633290bb95d65c197761c0746652c037038
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-09-17 17:42:25 -0300

    :sparkles: Add app bar and menu icon.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.s?css$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -180,6 +180,7 @@ module.exports = {
                         sourceMap: shouldUseSourceMap,
                       },
                     },
+                    {loader: require.resolve('sass-loader')},
                     {
                       loader: require.resolve('postcss-loader'),
                       options: {
