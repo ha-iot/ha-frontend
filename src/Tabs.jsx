0d44f39e596ebeb8a2c3e0d9b6f17090f46ee194
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-09-17 21:36:04 -0300

    :sparkles: Make it nice to switch tabs.

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import {Tabs, Tab} from 'material-ui'
+import SwipeableViews from 'react-swipeable-views'
 
 import LampsView from './LampsView'
 import GeneralView from './GeneralView'
 
 export default class MenuTabs extends React.Component {
+  state = {
+    slideIndex: 0
+  }
+
+  handleChange = (value) => {
+    this.setState({slideIndex: value})
+  }
+
   render() {
     return (
-      <Tabs>
-        <Tab label="Lâmpadas">
+      <div>
+        <Tabs onChange={this.handleChange} value={this.state.slideIndex}>
+          <Tab label="Lâmpadas" value={0}/>
+          <Tab label="Geral" value={1}/>
+        </Tabs>
+        <SwipeableViews index={this.state.slideIndex} onChangeIndex={this.handleChange}>
           <LampsView lamps={this.props.lamps}/>
-        </Tab>
-        <Tab label="Geral">
           <GeneralView lamps={this.props.lamps}/>
-        </Tab>
-      </Tabs>
+        </SwipeableViews>
+      </div>
     )
   }
 }
