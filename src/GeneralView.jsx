59358a55d4bdbdfdc29e4efcdf95b9b3e16a58ba
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-09-27 02:37:31 -0300

    :sparkles: Add general views.

--- src/GeneralView.jsx
+++ src/GeneralView.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import {RaisedButton} from 'material-ui'
+import {DeviceBrightnessHigh, DeviceBrightnessLow} from 'material-ui/svg-icons/index'
 
-export default class GeneralView extends React.Component {
+import './GeneralView.scss'
+import socket from './socket'
+
+export default class LampsView extends React.Component {
   render() {
+    let lamps
+    const hasLamps = !!this.props.lamps.length
+    const height = '4em'
+
+    if (hasLamps) {
+      lamps = this.props.lamps.map((lamp, i) => {
+        let [LampIcon, color] = lamp.isOn ? [DeviceBrightnessHigh, '#d8d800'/* yellow */] : [DeviceBrightnessLow, 'grey']
+        return <LampIcon key={i} className="general-view__lamps__lamp" style={{color, height}}/>
+      })
+    } else {
+      lamps = <span>Não há lâmpadas conectadas.</span>
+    }
+
+    const actionButtons = [
+      {action: 'toggle', label: 'Alternar lâmpadas'},
+      {action: 'on', label: 'Ligar lâmpadas'},
+      {action: 'off', label: 'Desligar lâmpadas'},
+    ]
+    const actionFactory = action => () => {
+      socket.emit('client/lampsAction', {target: 'all', action})
+    }
+    const buttons = actionButtons.map(({action, label}) =>
+      <RaisedButton className="general-view__buttons__button" onClick={actionFactory(action)} label={label} primary={true} disabled={!hasLamps} style={{height}}/>
+    )
+
     return (
-      <div/>
+      <div className="general-view">
+        <div className="general-view__lamps">
+          {lamps}
+        </div>
+        <div className="general-view__buttons">
+          {buttons}
+        </div>
+      </div>
     )
   }
 }
