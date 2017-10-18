75b2a3b4d277253a363387b1ab12df100459ca21
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-10-18 08:43:54 -0300

    :sparkles: :umbrella: Add first tests and Travis config file.

---
+++ src/components/Card.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './Card'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Card primaryData="Primary Info" secondaryData="Secondary Info" label="Label"/>, div)
+})
---
+++ src/containers/NoData.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import NoData from './NoData'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<NoData/>, div)
+})
